ecf4fdea74900217d2ff486768dffb85e99513d2
Matt Wolffe <wolffemf@dukes.jmu.edu>
2024-06-08 12:48:35 -0400

    just style/anim things

--- localtoast/frontend/src/pages/home.jsx
+++ localtoast/frontend/src/pages/home.jsx
@@ -1,22 +1,30 @@
 import { useSpring, animated } from '@react-spring/web'
 import { useTrail } from '@react-spring/web'
 import { useState } from "react";
+import { NavLink as Link } from "react-router-dom";
 
 import toast from "./../media/toast.png"
 
 // const DarkMagick = <span className='medievalsharp-regular'>dark magick</span>
 const DarkMagick = <span className='jacquard-12-regular'>dark magick</span>
+const IsLoggedIn = <animated.p className="ubuntu-light">You are logged in as <animated.span className="ubuntu-bold">{username}</animated.span></animated.p>
+
+const IsNotLoggedIn = (
+  <animated.div>
+    <animated.p className="ubuntu-light">You are not logged in. <Link to="/">Why make an account?</Link></animated.p></animated.div>
+)
 
 const PlainToast      = <animated.img src={toast}></animated.img>
-const SiteName        = <animated.h1 className="indie-flower-regular splash">Local Toast</animated.h1>
+const SiteName        = <animated.h1 className="monoton-regular splash">Local Toast</animated.h1>
+// const SiteName        = <animated.h1 className="indie-flower-regular splash">Local Toast</animated.h1>
 const SiteSummary     = <animated.h2 className="ubuntu-light splash">Find toast(s) near you using { DarkMagick }</animated.h2>
-const LoggedIn        = <animated.p className="ubuntu-light">{username ? `You are logged in as ${username}` : "You are not logged in" }</animated.p>
+// const LoggedIn        = <animated.p className="ubuntu-light">{username ? `You are logged in as ${username}` : "You are not logged in" }</animated.p>
 const SiteDescription = <animated.p className="ubuntu-light">This is Matt here, tinkering with some stuff, ya know, as I do. Should I keep typing? probably. I guess this will be a larger site summary/description</animated.p>
 
 export default function Home() {
   const [toggle, set] = useState(true);
   
-  const items = [SiteName, SiteSummary, PlainToast,  SiteDescription, LoggedIn];
+  const items = [SiteName, SiteSummary, PlainToast,  SiteDescription, username ? IsLoggedIn : IsNotLoggedIn];
   const config = { mass: 10, tension: 2000, friction: 200 };
 
   const trail = useTrail(items.length, {
