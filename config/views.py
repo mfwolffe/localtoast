9e9dd593c708f4e75c5d637e13befd409bdca108
Matt Wolffe <wolffemf@dukes.jmu.edu>
2024-10-18 17:53:20 -0400

    allow site access sans account

--- config/views.py
+++ config/views.py
@@ -7,7 +7,7 @@ from django.views import View
 from django.views.generic import TemplateView
 
 
-class HomeView(LoginRequiredMixin, TemplateView):
+class HomeView(TemplateView):
     template_name = "index.html"
 
     def get_context_data(self, **kwargs):
