fefbfdc4c091b3f50ad28454842fa763b7f5f97c
Matt Wolffe <wolffemf@dukes.jmu.edu>
2024-10-19 04:23:51 -0400

    cleanup, refacor dash

--- localtoast/frontend/src/App.jsx
+++ localtoast/frontend/src/App.jsx
@@ -1,12 +1,6 @@
 import Home from './pages/home';
 import SearchBar from './pages/search';
-import { Button } from 'react-bootstrap';
-
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import './styles/bootstrap.min-toast.css'
 import './App.css'
@@ -14,9 +8,6 @@ import './styles/fonts.css'
 
 // function App({username}) {
 function App() {
-  console.log("username is ", username);
-  console.log(window);
-
   return (
     <Router>
       <Routes>
